e51bcf2f16ef04748d6fd9365ad4479e69b81193
green-pea-ai <yachtboatfor@gmail.com>
2021-01-15 20:05:56 +0900

    20:05, 모델 생성, make migrations

--- DjangoRestApisPSQL/restapi/models.py
+++ DjangoRestApisPSQL/restapi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Wdmodel(models.Model):
+	title = models.CharField(max_length=100, blank=False, default='')
+	description = models.CharField(max_length=200, blank=False, default='')
+	published = models.BooleanField(default=False)
